7bee3c7873a32ca691ce85591c117b6d9b036a35
lwt <lianwt115@163.com>
2018-07-12 17:40:22 +0800

    Signed-off-by: lwt <lianwt115@163.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import  ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './login/Login';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Switch , BrowserRouter, Route,Redirect} from 'react-router-dom';
+
+class Index extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/main"  exact  component={App}/>
+                    <Route path="/login" component={Login} />
+                    <Redirect from="/" to="/login"/>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
+}
+
+ReactDOM.render( <Index/>, document.getElementById('root'));
 registerServiceWorker();
